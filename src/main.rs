b5e7adffdf02791187500bfbb3a22fc4520359d6
Ettienne Scharneck <ettiennescharneck@gmail.com>
2022-12-06 10:34:47 +0200

    day 6

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,14 @@
 fn main() {
-    solutions::day5::part1();
-    solutions::day5::part2();
+    // solutions::day1::part1();
+    // solutions::day1::part2();
+    // solutions::day2::part1();
+    // solutions::day2::part2();
+    // solutions::day3::part1();
+    // solutions::day3::part2();
+    // solutions::day4::part1();
+    // solutions::day4::part2();
+    // solutions::day5::part1();
+    // solutions::day5::part2();
+    solutions::day6::part1();
+    solutions::day6::part2();
 }
---
+++ src/solutions/day6.rs
@@ -0,0 +1,49 @@
+use std::collections::{HashSet, VecDeque};
+
+// https://adventofcode.com/2022/day/5
+use crate::helpers::input_parser::InputParser;
+
+pub fn part1() {
+    print!("Day6 Part 1 Solution: ");
+    find_marker(4);
+}
+
+pub fn part2() {
+    print!("Day6 Part 2 Solution: ");
+    find_marker(14);
+}
+
+fn find_marker(packet_size: usize) {
+    let parser = InputParser::new("src/input/day6.txt");
+    for line in parser.get_string_vec() {
+        let mut buffer: VecDeque<char> = VecDeque::new();
+
+        let mut i = 0;
+        for c in line.chars() {
+            i += 1;
+            buffer.push_back(c);
+            if buffer.len() == packet_size {
+                let mut unique_set: HashSet<char> = HashSet::new();
+                let mut unique = true;
+                for b in buffer.clone().iter() {
+                    if !unique_set.insert(*b) {
+                        unique = false;
+                        break;
+                    }
+                }
+
+                if unique {
+                    print!("Found unique sequence at: {} Sequence: ", i);
+                    for c in buffer.iter() {
+                        print!("{}", c);
+                    }
+                    break;
+                } else {
+                    buffer.pop_front();
+                }
+            }
+        }
+        buffer.clear();
+        println!();
+    }
+}
